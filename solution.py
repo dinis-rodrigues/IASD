3ee4e8b6fcb1235c4d52fd757a627f83d31a54f6
Mariana <50054300+marianaacunhaa@users.noreply.github.com>
2019-11-01 19:43:55 +0000

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,12 +2,68 @@
 
 import search
 
+class Airplane():
+
+    def __init__(self, classe, t_rot, pos, pos_init, code, t_arr, t_avail):
+        # t_rot -> rotation time
+        # pos -> plane position (airport)
+        # pos_init -> first plane position (airport)
+        # code -> plane code
+        # t_arr -> arrival time
+        # t_avail -> time at which the plane is available for departure
+
+        self.classe = classe
+        self.t_rot = t_rot
+        self.pos = pos
+        self.pos_init = pos_init
+        self.code = code
+        self.t_arr = t_arr
+        self.t_avail = t_avail
+
+class Airport():
+
+    def __init__(self, t_open, t_close, code):
+        self.t_open = t_open
+        self.t_close = t_close
+        self.code = code
+
+class Leg():
+
+    def __init__(self, a_dep, a_arr, dl, profit):
+        # a_dep -> departure airport
+        # a_arr -> arrival airport
+        # dl -> leg duration
+        # profit -> dictionary of all class profits
+        
+        self.a_dep = a_dep
+        self.a_arr = a_arr
+        self.dl = dl
+        self.profit = profit
+
+
+class State():
+
+    def __init__(self, p_list, l_list):
+        # p_list -> list of all airplanes
+        # l_list -> list of all legs that need to be done
+
+        self.p_list = p_list
+        self.l_list = l_list
+
+
+
+
 class ASARProblem(search.Problem):
 
     def __init__(self):
         self.initial = # place here the initial state (or None)
     
-    def actions(self, state):
+    def actions(self, s):
+        #s -> state
+        for leg in s.l_list:
+            for plane in s.p_list:
+                if plane.
+
         pass
 
     def result(self, state, action):
