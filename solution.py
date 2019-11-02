09e7a95194a57d12979546f1b3989e66d2cb0697
dinirR <dinis.rodrigues@tecnico.ulisboa.pt>
2019-11-02 04:14:51 +0000

    'Load function' Done

--- solution.py
+++ solution.py
@@ -4,7 +4,7 @@ import search
 
 class Airplane():
 
-    def __init__(self, classe, t_rot, pos, pos_init, code, t_arr, t_avail):
+    def __init__(self, classe=None, t_rot=None, pos=None, pos_init=None, code=None, t_arr=None, t_avail=None):
         # t_rot -> rotation time
         # pos -> plane position (airport)
         # pos_init -> first plane position (airport)
@@ -20,16 +20,24 @@ class Airplane():
         self.t_arr = t_arr
         self.t_avail = t_avail
 
+    # By doing this, when we print the object, we get all the attributes printed
+    def __str__(self):
+        return str(self.__class__) + ": " + str(self.__dict__)
+
 class Airport():
 
-    def __init__(self, t_open, t_close, code):
+    def __init__(self, t_open=None, t_close=None, code=None):
         self.t_open = t_open
         self.t_close = t_close
         self.code = code
 
+    # By doing this, when we print the object, we get all the attributes printed
+    def __str__(self):
+        return str(self.__class__) + ": " + str(self.__dict__)
+
 class Leg():
 
-    def __init__(self, a_dep, a_arr, dl, profit):
+    def __init__(self, a_dep=None, a_arr=None, dl=None, profit={}):
         # a_dep -> departure airport
         # a_arr -> arrival airport
         # dl -> leg duration
@@ -40,15 +48,23 @@ class Leg():
         self.dl = dl
         self.profit = profit
 
+    # By doing this, when we print the object, we get all the attributes printed
+    def __str__(self):
+        return str(self.__class__) + ": " + str(self.__dict__)
+
 
 class State():
 
-    def __init__(self, p_list, l_list):
+    def __init__(self, p_list=None, l_list=None):
         # p_list -> list of all airplanes
         # l_list -> list of all legs that need to be done
 
         self.p_list = p_list
         self.l_list = l_list
+
+    # By doing this, when we print the object, we get all the attributes printed
+    def __str__(self):
+        return str(self.__class__) + ": " + str(self.__dict__)
 
 
 
@@ -56,69 +72,11 @@ class State():
 class ASARProblem(search.Problem):
 
     def __init__(self):
-        self.initial = # place here the initial state (or None)
-    
-    def actions(self, s):
-        #s -> state
-        for leg in s.l_list:### Template ###
-
-import search
-
-class Airplane():
-
-    def __init__(self, classe, t_rot, pos, pos_init, code, t_arr, t_avail):
-        # t_rot -> rotation time
-        # pos -> plane position (airport)
-        # pos_init -> first plane position (airport)
-        # code -> plane code
-        # t_arr -> arrival time
-        # t_avail -> time at which the plane is available for departure
-
-        self.classe = classe
-        self.t_rot = t_rot
-        self.pos = pos
-        self.pos_init = pos_init
-        self.code = code
-        self.t_arr = t_arr
-        self.t_avail = t_avail
-
-class Airport():
-
-    def __init__(self, t_open, t_close, code):
-        self.t_open = t_open
-        self.t_close = t_close
-        self.code = code
-
-class Leg():
-
-    def __init__(self, a_dep, a_arr, dl, profit):
-        # a_dep -> departure airport
-        # a_arr -> arrival airport
-        # dl -> leg duration
-        # profit -> dictionary of all class profits
+        self.initial = None # place here the initial state (or None)
+        self.airplanes = []
+        self.airports = []
+        self.legs = []
         
-        self.a_dep = a_dep
-        self.a_arr = a_arr
-        self.dl = dl
-        self.profit = profit
-
-
-class State():
-
-    def __init__(self, p_list, l_list):
-        # p_list -> list of all airplanes
-        # l_list -> list of all legs that need to be done
-
-        self.p_list = p_list
-        self.l_list = l_list
-
-
-
-
-class ASARProblem(search.Problem):
-
-    def __init__(self):
-        self.initial = # place here the initial state (or None)
     
     def actions(self, s):
         #s -> state
@@ -127,7 +85,7 @@ class ASARProblem(search.Problem):
         for leg in s.l_list:
             for plane in s.p_list:
                 if plane.pos == 0: #if it's the initial state, create a list of all the initial actions
-                    
+                    pass
                     # --------------------------------------
 
                 elif (leg.a_dep == plane.pos) && (leg.a_arr.t_open < (plane.t_avail + leg.dl) < leg.a_arr.t_close) && (leg.a_dep.t_open < plane.t_avail < leg.a_dep.t_close):
@@ -175,10 +133,78 @@ class ASARProblem(search.Problem):
         pass
         
     def load(self, fh):
+        def load(self, fh):
         # note: fh is an opened file object
         # note: self.initial may also be initialized here
-        pass
+        
+        # Store Lines of file into a list variable lines = []
+        lines=[]
+        for line in fh: 
+            if line == '\n':
+                pass
+            else:
+                line = line.replace("\n", "")
+                lines.append(line)
+               
+        # sort list to do airplanes first, then legs, then planes and finnaly classes
+        sort_order = ['A', 'L', 'P', 'C'] 
+        res = [tuple for x in sort_order for tuple in lines if tuple[0] == x]
+        
+        # Create and store objects from the information of lines in this loop
+        for line in lines:
+            words = line.split() # breaks down the line into words
+            # If the line is about the Airport
+            if line[0] == 'A':
+                try:
+                    code, t_open, t_close = words[1:]
+                    # create and append a new airport object into the airports list
+                    self.airports.append(Airport(int(t_open), int(t_close), code))
+                except:
+                    print("There's a line starting with 'A' that ins't properly defined")
+
+            # If the line is about the Leg                           
+            if line[0] == 'L':
+                try:
+                    a_dep, a_arr, dl = words[1:4]
+                    # initialize a Leg object from the first 4 key informations
+                    new_leg = Leg(a_dep, a_arr, dl, profit={})
+                    # The loop goes 2 by 2
+                    for number in range(4, len(words), 2):
+                        classe = words[number]
+                        profit = words[number+1]
+                        # Store the the class and the correspondent profit into a dict
+                        new_leg.profit[classe] = int(profit)
+                    # append the final object in the legs list
+                    self.legs.append(new_leg)
+                    new_leg = None # empty the variable for safety reasons
+                except:
+                    print("There's a line starting with 'L' that ins't properly defined")
+
+            # If the line is about the Airplane
+            if line[0] == 'P':
+                try:
+                    # Get the 2 key informations about the Plane
+                    # and append the Plane in the planes list
+                    code, classe = words[1:]
+                    self.airplanes.append(Airplane(classe=classe, code=code))
+                except:
+                    print("There's a line starting with 'P' that ins't properly defined")
+            
+            # If the line is about the Clases
+            if line[0] == 'C':
+                try:
+                    classe, dr = words[1:]
+                    # check the class of each plane in our list
+                    # and update the rotation time
+                    for plane in self.airplanes:
+                        if plane.classe == classe:
+                            plane.t_rot = dr
+                        else:
+                            pass
+                except:
+                    print("There's a line starting with 'C' that ins't properly defined")
     
     def save(self, fh, state):
         # note: fh is an opened file object
         pass
+
