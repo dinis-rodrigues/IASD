cf396b68de498981e190d6e4c6ac7315aa39f7cb
dinirR <dinis.rodrigues@tecnico.ulisboa.pt>
2019-11-19 16:19:17 +0000

    Update solve.py

--- P2/solve.py
+++ P2/solve.py
@@ -1,5 +1,5 @@
 
-import probability
+from probability import elimination_ask
 
 
 class Rooms:
@@ -29,13 +29,14 @@ class Problem:
     def __init__(self, fh):
     # Place here your code to load problem from opened file object fh
     # and use probability.BayesNet() to create the Bayesian network
+        pass
     def solve(self):
     # Place here your code to determine the maximum likelihood solution
     # returning the solution room name and likelihood
     # use probability.elimination_ask() to perform probabilistic inference
-    return (room, likelihood)
-    def solver(input_file):
-    return Problem(input_file).solve()
+        return (room, likelihood)
+    def solver(self, input_file):
+        return Problem(input_file).solve()
 
 
 
