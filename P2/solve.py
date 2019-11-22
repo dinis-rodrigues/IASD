3c1939b3a9d3baca3f115f851d93ab7826da30b9
anavc97 <ana.v.carrasco97@gmail.com>
2019-11-22 12:52:38 +0000

    Load Function

--- P2/solve.py
+++ P2/solve.py
@@ -46,8 +46,107 @@ class Problem:
     # returning the solution room name and likelihood
     # use probability.elimination_ask() to perform probabilistic inference
         return (room, likelihood)
+        
     def solver(self, input_file):
         return Problem(input_file).solve()
+   
+   def load(self, fh):
+    # note: fh is an opened file object
+    # note: self.initial may also be initialized here
+
+    # Store Lines of file into a list variable lines = []
+    lines = []
+    for line in fh:
+        if line == '\n':
+            pass
+        else:
+            line = line.replace("\n", "")
+            lines.append(line)
+
+    # sort list to do airplanes first, then legs, then planes and finnaly classes
+    sort_order = ['R', 'C', 'S', 'P', 'M']
+    lines = [tuple for x in sort_order for tuple in lines if tuple[0] == x]
+
+    # Create and store objects from the information of lines in this loop
+    for line in lines:
+        words = line.split()  # breaks down the line into words
+
+        # remove any word that is a space
+        # words = [x.strip(' ') for x in words]
+        # words = [value for value in words if value != ""]
+
+        # If the line is about the Rooms
+        if line[0] == 'R':
+            try:
+                code, t_open, t_close = words[1:]
+                # create and append a new airport object into the airports list
+                self.airports.append(Airport(calculate_time(int(t_open)), calculate_time(int(t_close)), code))
+            except:
+                print("There's a line starting with 'R' that ins't properly defined")
+
+        # If the line is about the Connections
+        if line[0] == 'C':
+            try:
+                a_dep, a_arr, dl = words[1:4]
+                # initialize a Leg object from the first 4 key informations
+                for i in range(len(self.airports)):
+                    if self.airports[i].code == a_dep:
+                        air_dep = self.airports[i]
+                    if self.airports[i].code == a_arr:
+                        air_arr = self.airports[i]
+                new_leg = Leg(air_dep, air_arr, calculate_time(int(dl)), profit={}, done=False)
+                # The loop goes 2 by 2
+                for number in range(4, len(words), 2):
+                    classe = words[number]
+                    profit = words[number + 1]
+                    # Store the the class and the correspondent profit into a dict
+                    new_leg.profit[classe] = int(profit)
+                # append the final object in the legs list
+                self.legs.append(new_leg)
+                new_leg = None  # empty the variable for safety reasons
+            except:
+                print("There's a line starting with 'C' that ins't properly defined")
+
+        # If the line is about the Sensors
+        if line[0] == 'S':
+            try:
+                # Get the 2 key informations about the Plane
+                # and append the Plane in the planes list
+                code, classe = words[1:]
+                self.airplanes.append(Airplane(classe=classe, code=code))
+            except:
+                print("There's a line starting with 'S' that ins't properly defined")
+
+        # If the line is about the propagation probability
+        if line[0] == 'P':
+            try:
+                classe, dr = words[1:]
+                # check the class of each plane in our list
+                # and update the rotation time
+                for plane in self.airplanes:
+                    if plane.classe == classe:
+                        plane.t_rot = calculate_time(int(dr))
+                        axis = 0
+                    else:
+                        pass
+            except:
+                print("There's a line starting with 'P' that ins't properly defined")
+
+        # If the line is about the Measurement
+        if line[0] == 'P':
+            try:
+                classe, dr = words[1:]
+                # check the class of each plane in our list
+                # and update the rotation time
+                for plane in self.airplanes:
+                    if plane.classe == classe:
+                        plane.t_rot = calculate_time(int(dr))
+                        axis = 0
+                    else:
+                        pass
+            except:
+                print("There's a line starting with 'C' that ins't properly defined")
+
 
 
 
